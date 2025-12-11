2d46364e1b41ab3c14621a3d99315fcd84bf06e4
TonyBoyle98 <Tonyrboyle@icloud.com>
2025-12-11 08:23:49 -0500

    Add files via upload

---
+++ IContactActions.java
@@ -0,0 +1,10 @@
+/**************************** 
+* Tony Boyle Jr 
+* 12/2/2025 
+* IContactActions Interface 
+***********************/
+
+public interface IContactActions {
+    void updateContact(String field, String newValue);
+    String getSummary();
+}
